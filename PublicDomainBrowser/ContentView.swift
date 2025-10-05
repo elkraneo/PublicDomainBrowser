f8c80893c3685c07097726828f0a2dedcac7e18a
Cristian Díaz <cristian@210x7.io>
2025-10-05 22:50:59 +0200

    chore: bootstrap project

---
+++ PublicDomainBrowser/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PublicDomainBrowser
+//
+//  Created by Cristian Díaz on 05.10.25.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ PublicDomainBrowser/PublicDomainBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  PublicDomainBrowserApp.swift
+//  PublicDomainBrowser
+//
+//  Created by Cristian Díaz on 05.10.25.
+//
+
+import SwiftUI
+
+@main
+struct PublicDomainBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
